68c4fae583b9b40a202c0b621cd8013e65fea215
Diego Salazar <diego@greyrobot.com>
2016-09-29 15:30:42 -0400

    fix wrong method name

--- lib/action_cable/subscription_adapter/custom_adapter.rb
+++ lib/action_cable/subscription_adapter/custom_adapter.rb
@@ -3,7 +3,7 @@ require "action_cable/subscription_adapter/redis"
 # Override ActionCable's Redis adapter to use a configurable adapter
 module ActionCable::SubscriptionAdapter
   class CustomAdapter < Redis
-    def redis_connector
+    def redis_connection
       r=ActionCableSubscriptionAdapter.redis_connector.call self
       Rails.logger.info "ActionCableSubscriptionAdapter: #{r}"
       r
