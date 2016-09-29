c726d0cff2ff32fd4bbce6521b7c7397f244ae1e
Diego Salazar <diego@greyrobot.com>
2016-09-29 17:08:32 -0400

    added back support for assigning a proc to redis_connector

--- lib/action_cable/subscription_adapter/custom_adapter.rb
+++ lib/action_cable/subscription_adapter/custom_adapter.rb
@@ -4,7 +4,9 @@ require "action_cable/subscription_adapter/redis"
 module ActionCable::SubscriptionAdapter
   class CustomAdapter < Redis
     def redis_connection
-      ActionCableSubscriptionAdapter.redis_connector
+      redis = ActionCableSubscriptionAdapter.redis_connector
+      redis = redis.call self if redis.is_a? Proc
+      redis
     end
   end  
 end
